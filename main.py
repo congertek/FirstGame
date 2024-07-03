84d052732d93d1d4a4b0c8bbce83ac2c1515efbc
Kelly Conger <OldManCoder@CongerTek.com>
2024-07-03 13:25:31 -0400

    oldmancoder first commit from windows

---
+++ main.py
@@ -0,0 +1,54 @@
+import pygame
+from random import randint
+from os.path import join
+
+# General setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Shooter')
+running = True
+clock = pygame.time.Clock()
+
+# Plain Surface
+surf = pygame.Surface((100, 200))
+surf.fill('green')
+x = 100
+
+# Imports
+player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_surface.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = pygame.math.Vector2(2,-1)
+player_speed = 10
+
+star_surface = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(50)]
+
+meteor_surface = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surface.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surface = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surface.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
+
+while running:
+    clock.tick(30)
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw the game
+    display_surface.fill('grey20')
+    for pos in star_positions:
+        display_surface.blit(star_surface, pos)
+
+    display_surface.blit(meteor_surface, meteor_rect)
+    display_surface.blit(laser_surface, laser_rect)
+
+    # Player Movement
+    player_rect.center += player_direction * player_speed
+    display_surface.blit(player_surface, player_rect)
+
+    pygame.display.update()
+
+pygame.quit()
